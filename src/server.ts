1f0363bf3dfa3a5d928950d0f93e93b7efbd83fc
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-20 16:31:35 +0900

    feat: Patternlar

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-console.log("EXECUTED");
+// console.log("EXECUTED");
 
-import moment from 'moment';
+// import moment from 'moment';
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
 
 
-const person: string = "Rio";
-const count: number = 200;
+// const person: string = "Rio";
+// const count: number = 200;
+
+// Architectural pattern: MVC, Dependency Injection, MVP
+
+// MVC = MODEL VIEW CONTROLLER
+
+// Design pattern: Middleware, Decotar
