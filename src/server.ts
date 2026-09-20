730889c31dbadf3a8de32972b1b2d2bf2372cea1
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-20 16:18:09 +0900

    BRR: InitInitial Setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+console.log("EXECUTED");
+
+import moment from 'moment';
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+
+const person: string = "Rio";
+const count: number = 200;
