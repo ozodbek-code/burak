d8b3e164d65bdd13eb41ac1f73f21b1929291142
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-23 02:16:34 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,2 @@
-console.log("EXECUTED");
-
-import moment from 'moment';
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-
-const person: string = "Rio";
-const count: number = 200;
-
-// Architectural pattern: MVC, Dependency Injection, MVP
-
-// MVC = MODEL VIEW CONTROLLER
-
-// Design pattern: Middleware, Decotar
+import dotenv from 'dotenv';
+dotenv.config();
