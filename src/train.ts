8da7e667f47a2b737142c5b32b4312133b675a28
Ozodbek <ozodbekamirov1188@gmail.com>
2026-09-23 03:13:23 +0900

    feat: MIT M-task

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,7 @@
-console.log("Hello World")
+function getSquareNumbers(numbers: number[]) {
+  return numbers.map(number => ({
+    number: number,
+    square: number ** 2
+  }));
+}
+console.log(getSquareNumbers([4, 6, 1, 9,23]));
